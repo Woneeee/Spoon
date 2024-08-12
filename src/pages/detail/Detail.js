c569108a759c7328f9d7e1e404f0c2a4af4ff0e9
Woneeee <a6885088@naver.com>
2024-08-12 13:15:53 +0900

    detail filter

--- src/pages/detail/Detail.js
+++ src/pages/detail/Detail.js
@@ -2,13 +2,16 @@ import { useEffect, useState } from "react";
 import { restaurant } from "../../api";
 import { useScrollTop } from "../../lib/useScrollTop";
 import { useParams } from "react-router-dom";
+import { Loading } from "../../components/Loading";
+import { Header } from "../../components/Header";
 
 export const Detail = () => {
   useScrollTop();
 
   const [resData, setResData] = useState();
+  const { id: placeId } = useParams();
+  const [placeData, setPlaceData] = useState();
   const [isLoading, setIsLoading] = useState(true);
-  const { id } = useParams();
 
   useEffect(() => {
     (async () => {
@@ -20,8 +23,11 @@ export const Detail = () => {
             },
           },
         } = await restaurant();
-
         setResData(resResult);
+
+        const placeResult = resResult.filter((res) => res.CON_UID === 414);
+        setPlaceData(placeResult);
+
         setIsLoading(false);
       } catch (error) {
         console.log(error);
@@ -30,6 +36,17 @@ export const Detail = () => {
   }, []);
 
   // console.log(resData);
+  console.log(placeData);
 
-  return <div>Detail</div>;
+  return (
+    <>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <>
+          <Header />
+        </>
+      )}
+    </>
+  );
 };
