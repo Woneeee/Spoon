a80d5022b06f26995e98bf20fbdb59c509496d88
Woneeee <a6885088@naver.com>
2024-08-12 15:46:59 +0900

    detail indexof

--- src/constant/imgUrl.js
+++ src/constant/imgUrl.js
@@ -1,1 +1,2 @@
 export const IMG_BASE_URL = "https://www.gyeongju.go.kr/upload/content/";
+export const IMG_WWW_URL = "https://www.gyeongju.go.kr";
--- src/pages/detail/Detail.js
+++ src/pages/detail/Detail.js
@@ -4,6 +4,25 @@ import { useScrollTop } from "../../lib/useScrollTop";
 import { useParams } from "react-router-dom";
 import { Loading } from "../../components/Loading";
 import { Header } from "../../components/Header";
+import styled from "styled-components";
+import { IMG_BASE_URL, IMG_WWW_URL } from "../../constant/imgUrl";
+
+export const PlaceContainer = styled.div`
+  width: 100%;
+  background-color: pink;
+  margin-top: 50px;
+  display: flex;
+`;
+
+export const ImgWrap = styled.div`
+  width: 33.33%;
+  height: 350px;
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`;
 
 export const Detail = () => {
   useScrollTop();
@@ -26,7 +45,7 @@ export const Detail = () => {
         setResData(resResult);
 
         const placeResult = resResult.filter((res) => res.CON_UID === 414);
-        setPlaceData(placeResult);
+        setPlaceData(placeResult[0]);
 
         setIsLoading(false);
       } catch (error) {
@@ -35,8 +54,16 @@ export const Detail = () => {
     })();
   }, []);
 
-  // console.log(resData);
+  const firtImgSIdx = placeData?.CON_CONTENT.indexOf("/upload");
+  const firtImgEIdx = placeData?.CON_CONTENT.indexOf("jpg");
+  const secImgSIdx = placeData?.CON_CONTENT.lastIndexOf("/upload");
+  const secImgEIdx = placeData?.CON_CONTENT.lastIndexOf("jpg");
+
+  console.log(resData);
   console.log(placeData);
+  // console.log(secImgSIdx);
+  // console.log(secImgEIdx);
+  // console.log(placeData?.CON_CONTENT.slice(secImgSIdx, secImgEIdx));
 
   return (
     <>
@@ -45,6 +72,34 @@ export const Detail = () => {
       ) : (
         <>
           <Header />
+
+          <PlaceContainer>
+            <ImgWrap>
+              <img
+                src={IMG_BASE_URL + placeData.CON_IMGFILENAME}
+                alt={placeData.CON_TITLE}
+              />
+            </ImgWrap>
+            <ImgWrap>
+              <img
+                src={`
+                  ${IMG_WWW_URL}${placeData.CON_CONTENT.slice(
+                  firtImgSIdx,
+                  firtImgEIdx
+                )}jpg`}
+                alt={placeData.CON_TITLE}
+              />
+            </ImgWrap>
+            <ImgWrap>
+              <img
+                src={`${IMG_WWW_URL}${placeData.CON_CONTENT.slice(
+                  secImgSIdx,
+                  secImgEIdx
+                )}jpg`}
+                alt={placeData.CON_TITLE}
+              />
+            </ImgWrap>
+          </PlaceContainer>
         </>
       )}
     </>
